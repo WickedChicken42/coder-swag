c0bdf3ea15402eb5d5160f089d5cf327d61c7d2b
James Ullom <jamesullom@Jamess-MacBook-Pro-2.local>
2018-08-29 15:58:15 -0600

    Created the Product VC and setup the CollectonView within it

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by James Ullom on 8/29/18.
+//  Copyright © 2018 Hammer of the Gods Software. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
